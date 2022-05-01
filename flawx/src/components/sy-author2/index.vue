1121c61e4ae15e22770f4b6f0bf18e80165c3a49
WhiteFlaw <71926259+WhiteFlaw@users.noreply.github.com>
2022-05-01 15:44:31 +0800

    重新封装的用户组件

--- flawx/src/components/sy-author2/index.vue
+++ flawx/src/components/sy-author2/index.vue
@@ -1,23 +1,16 @@
 <template>
-  <div class="article_author">
-    <div class="article_author_avatar">
-      <el-avatar :size="120" :src="article_author.avatar" />
+  <div class="author">
+    <div class="author_avatar">
+      <el-avatar :size="120" :src="author_avatar" />
     </div>
-    <div class="article_author_name">
-      <p v-text="authorname"></p>
+    <div class="author_name">
+      <p v-html="author_name"></p>
     </div>
-    <div class="article_author_age">
-      <p>{{ article_author.register_time.substring(0, 10) }}入驻</p>
-      <!-- 这里如果不事先在data.article_author里声明register_time会导致报错substring的对象不可为undefined,一开始以为要在请求完做处理再渲染,并不是这个原因. -->
+    <div class="author_age">
+      <p v-html="author_age"></p>
     </div>
-    <p class="article_author_totalView">
-      受访:{{ article_author.totalView }}次
-    </p>
-    <div class="article_author_tag">
-      <el-tag class="ml_2" type="success">{{ article_author.tag1 }}</el-tag>
-      <el-tag class="ml_2" type="success">{{ article_author.tag2 }}</el-tag>
-      <el-tag class="ml_2" type="warning">{{ article_author.tag3 }}</el-tag>
-      <el-tag class="ml_2" type="danger">{{ article_author.tag4 }}</el-tag>
+    <div class="author_tag">
+      <slot name="tag"></slot>
     </div>
   </div>
 </template>
@@ -26,35 +19,51 @@
 import { ref, reactive, onMounted, watch } from "vue";
 export default {
   props: {
-    authorname: {
+    author_avatar: {
+      type: String,
+      default: "",
+    },
+    author_name: {
       type: String,
     },
-    article_author: {
-      type: Object,
+    author_age: {
+      type: String,
     },
   },
   setup(props) {
-    const authorname = ref(props.authorname);
-    const article_author = ref(props.article_author);
+    const author_name = ref(props.author_name);
+    const author_avatar = ref(props.author_avatar);
+    const author_age = ref(props.author_age);
     watch(
-      () => props.authorname,
+      () => props.author_avatar,
       (newValue) => {
-        authorname.value = newValue;
+        console.log(newValue);
+        author_avatar.value = newValue;
       }
     );
+
     watch(
-      () => props.article_author,
+      () => props.author_name,
       (newValue) => {
-        article_author.value = newValue;
+        console.log(newValue);
+        author_name.value = newValue;
       }
     );
-    return { authorname, article_author };
+
+    watch(
+      () => props.author_age,
+      (newValue) => {
+        console.log(newValue);
+        author_age.value = newValue;
+      }
+    );
+    return { author_name, author_avatar, author_age };
   },
 };
 </script>
 
 <style scoped>
-.article_author {
+.author {
   position: relative;
   top: 0px;
   left: 0px;
@@ -64,57 +73,51 @@ export default {
   width: 320px;
 }
 
-.article_author_avatar {
+.author_avatar {
   position: absolute;
   top: 100px;
   left: 96px;
+  background-position: 50% 50%;
 }
 
-.article_author_name {
+.author_name {
   position: absolute;
   top: 250px;
-  left: 120px;
+  left: 115px;
+  padding-right: 10px;
+  padding-left: 10px;
+  border-radius: 20px;
+  line-height: 30px;
   font-size: 25px;
   letter-spacing: 1px;
   color: rgb(102, 102, 102);
+  background-color: rgb(235, 235, 235);
 }
 
-.article_author_age {
+.author_age {
   position: absolute;
-  top: 330px;
-  left: 90px;
+  top: 300px;
+  left: 80px;
+  height: 30px;
+  padding-right: 5px;
+  padding-left: 5px;
+  border-radius: 20px;
+  line-height: 30px;  
   font-size: 17px;
   letter-spacing: 1px;
   transform: scale(0.8);
   color: rgb(170, 170, 170);
+  background-color: rgb(235, 235, 235);
 }
 
-.article_author_totalView {
+.author_tag {
   position: absolute;
-  top: 360px;
-  left: 115px;
-  font-size: 17px;
-  transform: scale(0.8);
-  color: rgb(170, 170, 170);
-}
-
-.article_author_tag {
-  position: absolute;
-  top: 295px;
+  top: 350px;
   left: 60px;
-}
-
-.ml_2 {
-  margin-left: 8px;
-}
-
-.article_author_action {
-  position: absolute;
-  top: 230px;
-  left: 50px;
-}
-
-.article_author_action_agree {
-  margin-left: 60px;
+  height: 30px;
+  width: 200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
