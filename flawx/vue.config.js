8fe5b3f9a9c49e327c90121a8a8dc07f48a0e7e4
WhiteFlaw <71926259+WhiteFlaw@users.noreply.github.com>
2022-05-07 12:43:06 +0800

    前端代理解决跨域

--- flawx/vue.config.js
+++ flawx/vue.config.js
@@ -14,5 +14,20 @@ module.exports = {
             }
         }
     },
-    
+    //本地能跑打包后在本地不能跑也是跨域
+    //但是本地打包完能跑到了服务器不能, 只能说可能是跨域.
+/*     devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://8.130.48.246:3000/',//这里填入你要请求的接口的前缀
+                ws: true,//代理websocked
+                changeOrigin: true,//虚拟的站点需要更管origin
+                secure: false,     //是否https接口
+                pathRewrite: {
+                    '^/api': ''//重写路径
+                }
+            }
+        }
+    } */
+
 }
