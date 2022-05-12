847e197f439e2ca4c64222c95931fad4ef144d4b
WhiteFlaw <71926259+WhiteFlaw@users.noreply.github.com>
2022-05-12 09:09:21 +0800

    setup syntactic sugar

--- flawx/src/components/sy-carousel/index.vue
+++ flawx/src/components/sy-carousel/index.vue
@@ -37,115 +37,102 @@
   </div>
 </template>
 
-<script>
-import { reactive, ref, watch, onMounted } from "vue";
+<script setup>
+import { reactive, ref, onMounted, defineProps, computed } from "vue";
 import { useRouter } from "vue-router";
 
-export default {
-  props: {
-    height: {
-      type: [String, Number],
-      default: "",
-    },
-    width: {
-      type: [String, Number],
-      default: "",
-    },
+const props = defineProps({
+  height: {
+    type: [String, Number],
+    default: "",
   },
-  computed: {
-    size() {
-      let sy_carousel_width = `width: ${this.width}px`;
-      let sy_carousel_height = `height: ${this.height}px`;
-      let el_carousel_width = `width: ${this.width}px`;
-      let el_carousel_height = `${this.height * 0.89}px`;
-      return {
-        sy_carousel_width,
-        sy_carousel_height,
-        el_carousel_width,
-        el_carousel_height,
-      };
+  width: {
+    type: [String, Number],
+    default: "",
+  },
+});
+
+const size = computed(() => {
+  let sy_carousel_width = `width: ${props.width}px`;
+  let sy_carousel_height = `height: ${props.height}px`;
+  let el_carousel_width = `width: ${props.width}px`;
+  let el_carousel_height = `${props.height * 0.89}px`;
+  return {
+    sy_carousel_width,
+    sy_carousel_height,
+    el_carousel_width,
+    el_carousel_height,
+  };
+});
+
+const router = useRouter();
+const cardShow = ref(null);
+const cardShow2 = ref(null);
+
+let data = reactive({
+  sy_carousel_title_img: [
+    //不请求
+    {
+      url: "https://s3.bmp.ovh/imgs/2022/03/a8e2bb733453086b.png",
+      id: 1,
+      alt: "arrow_l",
+      active: true,
+      direction: "left",
     },
-  },
-  setup(props) {
-    const router = useRouter();
-    const cardShow = ref(null);
-    const cardShow2 = ref(null);
+    {
+      url: "https://s3.bmp.ovh/imgs/2022/03/2a6f0982c02436a8.png",
+      id: 2,
+      alt: "arrow_r",
+      active: false,
+      direction: "right",
+    },
+  ],
+  countTime: 3,
+  load: true,
+});
 
-    let data = reactive({
-      sy_carousel_title_img: [
-        //不请求
-        {
-          url: "https://s3.bmp.ovh/imgs/2022/03/a8e2bb733453086b.png",
-          id: 1,
-          alt: "arrow_l",
-          active: true,
-          direction: "left",
-        },
-        {
-          url: "https://s3.bmp.ovh/imgs/2022/03/2a6f0982c02436a8.png",
-          id: 2,
-          alt: "arrow_r",
-          active: false,
-          direction: "right",
-        },
-      ],
-      countTime: 3,
-      load: true,
-    });
+onMounted(() => {
+  xxx();
+});
 
-    onMounted(() => {
-      xxx();
-    });
+const xxx = () => {
+  data.countTime -= 1;
+  if (data.countTime == 0) {
+    data.load = false;
+    clearInterval(interval);
+  }
+};
 
-    const xxx = () => {
-      data.countTime -= 1;
-      if (data.countTime == 0) {
-        data.load = false;
-        clearInterval(interval);
-      }
-    };
+let interval = setInterval(xxx, 1000);
 
-    let interval = setInterval(xxx, 1000);
+function arrowClick(val) {
+  //$ref在setup内会无法获取元素的ref属性值(vue3中没有$refs这一说),ref 需要在dom渲染完成后才会有，在使用的时候确保dom已经渲染完成,即mounted和以后。
+  //在setup里"const 元素的ref属性值 = ref(null),然后return出去,再在mounted里"元素的ref属性值.value"即可
+  data.load = false;
+  if (val === "right") {
+    cardShow.value.next();
+  } else {
+    cardShow.value.prev();
+  }
+}
 
-    function arrowClick(val) {
-      //$ref在setup内会无法获取元素的ref属性值(vue3中没有$refs这一说),ref 需要在dom渲染完成后才会有，在使用的时候确保dom已经渲染完成,即mounted和以后。
-      //在setup里"const 元素的ref属性值 = ref(null),然后return出去,再在mounted里"元素的ref属性值.value"即可
-      data.load = false;
-      if (val === "right") {
-        cardShow.value.next();
-      } else {
-        cardShow.value.prev();
-      }
-    }
+function arrowClick2(val) {
+  //$ref在setup内会无法获取元素的ref属性值,ref 需要在dom渲染完成后才会有，在使用的时候确保dom已经渲染完成。
+  data.load = false;
+  if (val === "right") {
+    cardShow2.value.next();
+  } else {
+    cardShow2.value.prev();
+  }
+}
 
-    function arrowClick2(val) {
-      //$ref在setup内会无法获取元素的ref属性值,ref 需要在dom渲染完成后才会有，在使用的时候确保dom已经渲染完成。
-      data.load = false;
-      if (val === "right") {
-        cardShow2.value.next();
-      } else {
-        cardShow2.value.prev();
-      }
-    }
-
-    const toArticle = (params) => {
-      router.push({
-        path: "/article",
-        query: {
-          id: params,
-        },
-      });
-    };
-
-    return {
-      data,
-      cardShow,
-      cardShow2,
-      arrowClick,
-      arrowClick2,
-      toArticle,
-    };
-  },
+const toArticle = (params) => {
+  router.push({
+    path: "/article",
+    query: {
+      id: params,
+    },
+  });
 };
 </script>
 
