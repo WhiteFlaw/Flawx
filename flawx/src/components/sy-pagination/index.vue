a6ae1ec73c57761d7a7a2b5ba2d6df38240e9360
WhiteFlaw <71926259+WhiteFlaw@users.noreply.github.com>
2022-05-12 09:10:06 +0800

    setup syntactic sugar

--- flawx/src/components/sy-pagination/index.vue
+++ flawx/src/components/sy-pagination/index.vue
@@ -37,95 +37,81 @@
   </div>
 </template>
 
-<script>
-import { onUpdated, ref, reactive, watch } from "vue";
+<script setup>
+import {
+  onUpdated,
+  ref,
+  reactive,
+  defineProps,
+  defineEmits,
+  watchEffect,
+} from "vue";
 import { useRouter } from "vue-router";
 
-export default {
-  props: {
-    newArr: {
-      type: Array,
-      default:[]
-    },
-    user_article_content_page: {
-      type: Number,
-    },
-    size: {
-      type: String,
-    },
+const props = defineProps({
+  newArr: {
+    type: Array,
+    default: [],
   },
-  computed: {
-    size() {
-      return this.size ? `sy-pagination-${this.size}` : "";
-    },
+  user_article_content_page: {
+    type: Number,
   },
-
-  emits: ["deleteArticle"],
-  setup(props, { emit }) {
-    let data = reactive({
-      currentPage: 1,
-      user_article_content: "",
-    });
-    const router = useRouter();
-    const newArr = ref(props.newArr);
-    const user_article_content_page = ref(props.user_article_content_page);
-
-    onUpdated(() => {
-      //不在渲染完毕之后调用一下, pagination页内容显示不出来
-      currentChange();
-    }),
-      //分好页的信息
-      watch(
-        () => props.newArr,
-        (newValue, oldValue) => {
-          newArr.value = newValue;
-        }
-      );
-
-    //总页数
-    watch(
-      () => props.user_article_content_page,
-      (newValue, oldValue) => {
-        user_article_content_page.value = newValue;
-        //console.log(user_article_content_page.value);
-      }
-    );
-
-    const currentChange = () => {
-      data.user_article_content = newArr.value[data.currentPage - 1];
-      //console.log(data.currentPage);
-    };
-
-    const prevClick = () => {
-      data.currentPage - 1;
-      data.user_article_content = newArr.value[data.currentPage];
-      //console.log(data.currentPage);
-    };
-
-    const nextClick = () => {
-      data.currentPage + 1;
-      data.user_article_content = newArr.value[data.currentPage];
-    };
-
-    const toArticle = (params) => {
-      router.push(params);
-    };
-
-    const deleteArticle = (item) => {
-      emit("deleteArticle", item); //在这里成功传出id之后,父组件接收不需要传进去参数,直接调用函数,不用写形参,直接函数内输出id就可以拿到
-    };
-
-    return {
-      data,
-      newArr,
-      user_article_content_page,
-      toArticle,
-      currentChange,
-      prevClick,
-      nextClick,
-      deleteArticle,
-    };
+  size: {
+    type: String,
   },
+});
+
+const size = computed(() => {
+  return props.size ? `sy-pagination-${props.size}` : "";
+});
+
+const emit = defineEmits(["deleteArticle"]); //发送事件时使用的方法名要与这里的变量名一致
+
+let data = reactive({
+  currentPage: 1,
+  user_article_content: "",
+});
+
+const router = useRouter();
+const newArr = ref(props.newArr);
+const user_article_content_page = ref(props.user_article_content_page);
+
+onUpdated(() => {
+  //不在渲染完毕之后调用一下, pagination页内容显示不出来
+  currentChange();
+});
+
+watchEffect(() => {
+  newArr.value = props.newArr;
+});
+
+//总页数
+watchEffect(() => {
+  user_article_content_page.value = props.user_article_content_page;
+});
+
+const currentChange = () => {
+  //这里一定不要先给currentPage-1, 不然onMounted开局的那次调用会把第一页吞掉
+  data.user_article_content = newArr.value[data.currentPage - 1];
+  //console.log(data.currentPage);
+};
+
+const prevClick = () => {
+  data.user_article_content = newArr.value[data.currentPage - 1];
+  //console.log(data.currentPage);
+};
+
+const nextClick = () => {
+  data.currentPage + 1;
+  data.user_article_content = newArr.value[data.currentPage];
+};
+
+const toArticle = (params) => {
+  router.push(params);
+};
+
+const deleteArticle = (item) => {
+  emit("deleteArticle", item); //在这里成功传出id之后,父组件接收不需要传进去参数,直接调用函数,不用写形参,直接函数内输出id就可以拿到
 };
 </script>
 
