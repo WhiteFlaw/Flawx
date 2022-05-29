8e844a8a17083e7feeb1aa3f0dadaf57b1b23eaf
WhiteFlaw <71926259+WhiteFlaw@users.noreply.github.com>
2022-05-29 21:45:12 +0800

    global theme switch

--- flawx/src/App.vue
+++ flawx/src/App.vue
@@ -1,39 +1,12 @@
 <template>
-  <!-- 在此处写页面底部法律之类,不要用fixed固定定位 -->
+  <sy-nav-bar></sy-nav-bar>
   <router-view />
 </template>
-<script>
-export default {
-  setup() {
-
-  },
-};
+<script setup>
+import syNavBar from "./components/sy-navbar";
 </script>
 
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-#nav {
-  padding: 14px;
-  background-color: rgb(255, 255, 255);
-}
-
-#nav a {
-  font-weight: bold;
-  color: #161616;
-}
-
-#nav a.router-link-exact-active {
-  color: #636363;
-}
+@import "./util/global.css";
 </style>
